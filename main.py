2a38139d103e495962b37c915ef7c4313e00bed8
Ha Tang Hoang <dubon@192.168.2.4>
2024-06-25 20:21:53 +0700

    test lại github

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-# Calculate sum a, b
+# Bài tập Week 4
 
-def sum(a, b):
-    return a + b
-
-def multiply(a, b):
-    return a*b
-
-def substract(a, b):
-    return a - b
-
-def divide(a,b):
-    return a/b
-
-if __name__ == "__main__":
-    print(f"Total : {sum(1, 2)}")
-
-if __name__ == "__main__":
-    print(f"Total : {multiply(1, 2)}")
-
-if __name__ == "__main__":
-    print(f"Total : {substract(1, 2)}")
-
-if __name__ == "__main__":
-    print(f"Total : {divide(1, 2)}")
+def describe(a, b):
+    return a+b
